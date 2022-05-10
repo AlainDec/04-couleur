76c58e8d16673a2e284435a3c2f9b321877afbb1
unknown <jtwest@free.fr>
2022-05-10 12:23:51 +0200

    add project

--- App.js
+++ App.js
@@ -4,8 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.upperBlock}>
+        <View style={styles.sideBlock}>
+          <View style={styles.purple}>
+          </View>
+          <View style={styles.yellow}>
+          </View>
+          <View style={styles.pink}>
+          </View>
+        </View>
+        <View style={[styles.sideBlock, styles.rowDir]}>
+          <View style={styles.red}>
+          </View>
+          <View style={styles.blue}>
+          </View>
+        </View>
+      </View>
+      <View style={styles.lowerBlock}>
+      </View>
     </View>
   );
 }
@@ -13,8 +29,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  upperBlock: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  lowerBlock: {
+    flex: 1,
+    backgroundColor: 'lightgray'
+  },
+  sideBlock: {
+    flex: 1,
+  },
+  rowDir: {
+    flexDirection: 'row',
+  },
+  red: {
+    flex: 3,
+    backgroundColor: 'red'
+  },
+  blue: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  purple: {
+    flex: 3,
+    backgroundColor: 'purple'
+  },
+  yellow: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  pink: {
+    flex: 1,
+    backgroundColor: 'pink'
   },
 });
